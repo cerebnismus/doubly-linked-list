814c2f5ef2b6fa029ea14174235fc9086f092235
Oguzhan Ince <oguzhanlarca@pm.me>
2021-09-10 19:24:04 +0300

    Update Iterator.cpp

--- src/Iterator.cpp
+++ src/Iterator.cpp
@@ -1,10 +1,7 @@
 /**
  * @file Iterator.cpp
  * @description Iterator Implementation
- * @course 1/A
- * @assignment Ödev I
- * @date 14.08.2021
- * @author u161210071 - oguzhan.ince@ogr.sakarya.edu.tr
+ * @author oguzhan.ince@protonmail.com
  */
 
 #include <iostream>
